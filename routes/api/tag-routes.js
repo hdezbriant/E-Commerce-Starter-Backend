6215663159493d9d307948c0f992ec28054a29c1
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-04-25 14:08:57 -0700

    Corrected association in get All route. formatted Create new route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,7 +7,7 @@ const { Tag, Product, ProductTag } = require("../../models");
 router.get("/", async (req, res) => {
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product }],
+      include: [{ model: Product, through: ProductTag, as:"products" }],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -32,13 +32,14 @@ router.get("/:id", (req, res) => {
 });
 
 // create a new tag
-router.post("/", async (req, res) => {});
-try {
-  const tagData = await Tag.create(req.body);
-  res.status(200).json(tagData);
-} catch (err) {
-  res.status(400).json(err);
-};
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 // update a tag's name by its `id` value
 router.put('/:id', async (req, res) => {
