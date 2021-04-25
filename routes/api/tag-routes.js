1913312e1684f280d75a120700071cae8472bc95
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-04-25 13:14:25 -0700

    updated Create new route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -33,6 +33,12 @@ router.get("/:id", (req, res) => {
 
 // create a new tag
 router.post("/", async (req, res) => {});
+try {
+  const tagData = await Tag.create(req.body);
+  res.status(200).json(tagData);
+} catch (err) {
+  res.status(400).json(err);
+};
 
 // update a tag's name by its `id` value
 router.put("/:id", (req, res) => {});
