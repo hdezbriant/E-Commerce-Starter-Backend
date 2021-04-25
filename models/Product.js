56d933b5c602c1e7e9f07a75530bfbb91aea43e7
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-04-25 09:21:15 -0700

    Added remaining columns and fields.

--- models/Product.js
+++ models/Product.js
@@ -13,7 +13,33 @@ Product.init(
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
-    }
+    },
+    productName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      },
+    },
   },
   {
     sequelize,
