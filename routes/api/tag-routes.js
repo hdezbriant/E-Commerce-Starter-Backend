035ef0a7d4682324c982983c56a8eb04e03a0d3e
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-04-25 13:13:00 -0700

    updated get all route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,27 +1,34 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// find all tags
+router.get("/", async (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+// find a single tag by its `id`
+router.get("/:id", (req, res) => {
+
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+// create a new tag
+router.post("/", async (req, res) => {
+
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
+// update a tag's name by its `id` value
+router.put("/:id", (req, res) => {});
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
 });
 
