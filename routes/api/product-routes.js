f2001bda09bcc21f1ff9fdd25a5ac83cf29bb879
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-04-25 12:59:01 -0700

    Updated delete route with message response

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -118,7 +118,7 @@ router.delete("/:id", async (req, res) => {
       res.status(404).json({ message: 'No product with this id!' });
       return;
     }
-    res.status(200).json(productData);
+    res.status(200).json({productData, message:"Product successfully deleted!"});
   } catch (err) {
     res.status(500).json(err);
   }
