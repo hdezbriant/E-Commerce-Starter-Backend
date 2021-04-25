4e2f6f6a3990318778c3c88f5556f3c28b3d32e0
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-04-25 15:08:51 -0700

    Corrected syntax for Get by ID route, converted to async function

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,7 +7,7 @@ const { Tag, Product, ProductTag } = require("../../models");
 router.get("/", async (req, res) => {
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product, through: ProductTag, as:"products" }],
+      include: { model: Product, through: ProductTag, as:"products" },
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -16,10 +16,10 @@ router.get("/", async (req, res) => {
 });
 
 // find a single tag by its `id`
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
-    const tagData = Tag.findByPk(req.params.id, {
-      include: { model: Product },
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: { model: Product, through: ProductTag, as:"products" },
     });
     if (!tagData) {
       res.status(404).json({ message: "Tag ID not found!" });
